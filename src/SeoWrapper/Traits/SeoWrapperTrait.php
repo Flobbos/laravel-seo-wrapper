35474540b8c3e3e8f07bed1a0f763d32b79da553
Flobbo <Flobbo@192.168.4.3>
2017-05-16 13:21:28 +0200

    Added basic functionality for Seo Wrapper

--- src/SeoWrapper/Traits/SeoWrapperTrait.php
+++ src/SeoWrapper/Traits/SeoWrapperTrait.php
@@ -3,7 +3,30 @@
 namespace Flobbos\SeoWrapper\Traits;
 
 use Artesaos\SEOTools\Traits\SEOTools as SEOToolsTrait;
+use Illuminate\Support\Facades\Lang;
 
 trait SeoWrapperTrait {
+    
+    use SEOToolsTrait;
+    
+    public function setSeo($title, $slug, $meta_description = null){
+        
+        //Work on the title first
+        $trans_str = 'seo.' . str_slug($title) . '.title';
+        $title = Lang::has($trans_str) ? trans($trans_str) : $title;
+        $this->seo()->setTitle($title);
+        
+        //Check meta description
+        if(!is_null($meta_description)){
+            $this->seo()->setDescription($meta_description);
+        }
+        //Opengraph 
+        $this->seo()->opengraph()->addProperty('type', 'page');
+        $url = request()->path() == '/' ? '/' : $slug;
+        $this->seo()->opengraph()->setUrl(url($url) );
+        //Canonical
+        $this->seo()->setCanonical(request()->url());
+        
+    }
     
 }
