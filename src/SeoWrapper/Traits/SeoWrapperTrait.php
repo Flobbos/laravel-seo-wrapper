66c5c1fc9ebd80dc9046d37a418f508567f5c3b9
Alexej Krzewitzki <alexej.k@ultrabold.com>
2017-05-16 19:03:42 +0200

    Actually added some code! Wow!

--- src/SeoWrapper/Traits/SeoWrapperTrait.php
+++ src/SeoWrapper/Traits/SeoWrapperTrait.php
@@ -3,7 +3,27 @@
 namespace Flobbos\SeoWrapper\Traits;
 
 use Artesaos\SEOTools\Traits\SEOTools as SEOToolsTrait;
+use Illuminate\Support\Facades\Lang;
 
 trait SeoWrapperTrait {
+    
+    public function setSeo($title, $slug, $meta_description = null){
+        
+        //Process title first
+        $trans_str = 'seo.' . str_slug($title) . '.title';
+        $title = Lang::has($trans_str) ? trans($trans_str) : $title;
+        $this->seo()->setTitle($title);
+        
+        //Meta description
+        if(!is_null($meta_description)){
+            $this->seo()->setDescription($meta_description);
+        }
+        
+        //Opengraph
+        $url = request()->path() == '/' ? '/' : $slug;
+        $this->seo()->opengraph()->addProperty('type', 'page');
+        $this->seo()->opengraph()->setUrl( url($url) );
+
+    }
     
 }
